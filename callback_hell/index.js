a689b2d0834b3c44bf14d2ed1566bf347cd0cc6f
Abbas Asad <168946441+abbas-asad@users.noreply.github.com>
2024-07-30 23:43:30 +0500

    Add files via upload

---
+++ callback_hell/index.js
@@ -0,0 +1,26 @@
+// Callback Hell
+console.log("My");
+setTimeout(function () {
+    console.log("name");
+    setTimeout(function () {
+        console.log("is");
+        setTimeout(function () {
+            console.log("Abbas");
+            setTimeout(function () {
+                console.log("and");
+                setTimeout(function () {
+                    console.log("I");
+                    setTimeout(function () {
+                        console.log("am");
+                        setTimeout(function () {
+                            console.log("a");
+                            setTimeout(function () {
+                                console.log("Web Developer");
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
---
+++ callback_hell/index.ts
@@ -0,0 +1,27 @@
+// Callback Hell
+
+console.log("My");
+setTimeout(() => {
+  console.log("name");
+  setTimeout(() => {
+    console.log("is");
+    setTimeout(() => {
+      console.log("Abbas");
+      setTimeout(() => {
+        console.log("and");
+        setTimeout(() => {
+          console.log("I");
+          setTimeout(() => {
+            console.log("am");
+            setTimeout(() => {
+              console.log("a");
+              setTimeout(() => {
+                console.log("Web Developer");
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
