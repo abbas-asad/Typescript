6c2de66cf950277c40ab2636645916b97d30c2da
Abbas Asad <168946441+abbas-asad@users.noreply.github.com>
2024-07-30 19:09:38 +0500

    Add files via upload

---
+++ func-overload/index.ts
@@ -0,0 +1,12 @@
+// function overload
+function add(arg1: string, arg2: string): string;//option 1
+function add(arg1: number, arg2: number): number;//option 2
+function add(arg1: boolean, arg2: boolean): boolean;//option 3
+
+function add(arg1: any, arg2: any): any {
+    return arg1 + arg2;
+}
+//Calling 'add' with any other parameter types would cause an error except for the three options
+console.log(add(1, 2));
+console.log(add("Hello", "World"));
+console.log(add(true, false));
