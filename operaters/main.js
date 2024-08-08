d7cbf5862f149cbf88767d94ce19f7c8c273b82d
Abbas Asad <168946441+abbas-asad@users.noreply.github.com>
2024-08-09 02:40:57 +0500

    Add files via upload

---
+++ operaters/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// #1
+let numberOfStudents = 50000;
+let numberOfClasses = 21;
+let perBatchStudents = numberOfStudents / numberOfClasses;
+console.log(perBatchStudents);
+// #2
+let i = 10;
+let result = ++i + ++i + 6 + --i - 2;
+console.log(result);
+// #3
+let numberOfFaculty = 6;
+let numberOfManagement = 2;
+let numberOfBatches = 21;
+let numberOfTotalPersons = (numberOfFaculty + numberOfManagement) * numberOfBatches;
+console.log(numberOfTotalPersons);
